4c1f7951770a0b7dac54e318227963523a982a35
Nate Hill <nateh777@cs.utexas.edu>
2021-07-07 14:03:47 -0500

    Create dummy event handlers for buttons

---
+++ script.js
@@ -0,0 +1,39 @@
+function stepHandler() {
+    console.log("Step button pressed!");
+}
+
+function startHandler() {
+    console.log("Start button pressed!");
+}
+
+function freezeHandler() {
+    console.log("Freeze button pressed!");
+}
+
+function shutdownHandler() {
+    console.log("Shutdown button pressed!");
+}
+
+function liteHandler() {
+    console.log("Lite button pressed!");
+}
+
+function dataRecordOnHandler() {
+    console.log("Data Record On button pressed!");
+}
+
+function realTimeOnHandler() {
+    console.log("Real Time On button pressed!");
+}
+
+function dumpCheckpointHandler() {
+    console.log("Dump Checkpoint button pressed!");
+}
+
+function loadCheckpointHandler() {
+    console.log("Load Checkpoint button pressed!");
+}
+
+function exitHandler() {
+    console.log("Exit button pressed!");
+}
